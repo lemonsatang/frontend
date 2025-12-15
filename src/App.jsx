6ad9ed1930514dace922f2d149ddfd1e7144c084
JMS <jjjj9336@naver.com>
2025-12-15 11:32:02 +0900

    테이블 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 function Navbar() {
-  return <div>네브바</div>;
+  return (
+    <nav className="navbar navbar-expand-md bg-primary navbar-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand text-warning" href="#">
+          리액트 & 스프링부트
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <button className="btn btn-outline-light">유저 추가</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
